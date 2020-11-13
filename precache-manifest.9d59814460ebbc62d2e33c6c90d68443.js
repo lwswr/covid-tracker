0f6ec775f2ceaa3f7d66d962be0e3b471093ab40
Lewis Wear <lewiswear@Lewiss-MacBook-Pro.local>
2020-11-13 16:51:08 +0000

    Updates

---
+++ precache-manifest.9d59814460ebbc62d2e33c6c90d68443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87184f7588bc8b938b9cce962fe69caf",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "05637be640af267f6597",
+    "url": "/covid-tracker/static/css/main.0a70a2ea.chunk.css"
+  },
+  {
+    "revision": "f76b67b72e0ac45ccf19",
+    "url": "/covid-tracker/static/js/2.edc9791d.chunk.js"
+  },
+  {
+    "revision": "924d72d17eab6896b58e8070f8c2bc9e",
+    "url": "/covid-tracker/static/js/2.edc9791d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05637be640af267f6597",
+    "url": "/covid-tracker/static/js/main.6b3bd060.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
